ccfb69c67f196a7fae3ce4a8f834f07c9c744b88
heejeong min <hj.min1031@gmail.com>
2020-02-29 19:54:17 +0900

    init

---
+++ src/main/java/oi/algorithm/Solution.java
@@ -0,0 +1,19 @@
+package oi.algorithm;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Solution {
+    public static void main(String[] args) {
+        try (
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\hjmin\\Downloads\\input.txt"), StandardCharsets.UTF_8));
+        ) {
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
