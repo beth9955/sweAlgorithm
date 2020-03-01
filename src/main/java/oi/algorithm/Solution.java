ade210747a4e7b3f21486d20dc3f5906ba51c88e
heejeong min <hj.min1031@gmail.com>
2020-03-01 19:58:40 +0900

    1229 번

--- src/main/java/oi/algorithm/Solution.java
+++ src/main/java/oi/algorithm/Solution.java
@@ -10,14 +10,22 @@ import java.nio.charset.StandardCharsets;
 public class Solution {
 
     public static void main(String[] args) {
+        String file = "";
+//        file = "C:\\Users\\hjmin\\Downloads\\input1228.txt";
+        file = "C:\\Users\\hjmin\\Downloads\\input1229.txt";
+//        file = "C:\\Users\\hjmin\\Downloads\\input1230.txt";
+
         try (
-                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("C:\\Users\\hjmin\\Downloads\\input.txt"), StandardCharsets.UTF_8));
+                BufferedReader br = new BufferedReader
+                                        (new InputStreamReader
+                                            (new FileInputStream(file), StandardCharsets.UTF_8));
         ) {
 
             /** SW Expert Academy 3월 5일 **/
             March5th march5th = new March5th(br);
-            march5th.no1228();
-            //1291
+            //march5th.no1228();
+            march5th.no1229();
+            //march5th.no1230();
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/oi/algorithm/march/March5th.java
+++ src/main/java/oi/algorithm/march/March5th.java
@@ -1,7 +1,6 @@
-﻿package oi.algorithm.march;
+package oi.algorithm.march;
 
 import java.io.BufferedReader;
-import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,9 +16,10 @@ public class March5th {
 
     /**
      * SW Expert Academy 1228번
+     *
      * @throws Exception
      */
-    public void no1228 () throws Exception{
+    public void no1228() throws Exception {
         try {
             for (int i = 0; i < 10; i++) {
 
@@ -36,14 +36,14 @@ public class March5th {
                 }
 
                 /** 답 프린트 **/
-                System.out.printf("#%d", i+1);
-                for(int n=0; n < 10; n++){
+                System.out.printf("#%d", i + 1);
+                for (int n = 0; n < 10; n++) {
                     System.out.print(" " + result.get(n));
                 }
                 System.out.println();
 
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             throw e;
         }
@@ -51,6 +51,7 @@ public class March5th {
 
     /**
      * SW Expert Academy 1228번
+     *
      * @throws Exception
      */
     public static List<String> encrypt(List<String> result, String commands) throws Exception {
@@ -76,24 +77,127 @@ public class March5th {
 
     /**
      * SW Expert Academy 1229번
+     *
      * @throws Exception
      */
-    public void no1229 () throws Exception{
+    public void no1229() throws Exception {
         try {
+            for (int i = 0; i < 10; i++) {
 
-        } catch (Exception e){
+                /** 문제읽기 **/
+                int inital = Integer.parseInt(br.readLine());
+                Stream<String> stream = Stream.of(br.readLine().split(" "));
+                int commands = Integer.parseInt(br.readLine());
+                List<String> raw = Arrays.asList(br.readLine().split(" "));
 
+                /** 명령어를 insert와 delete로 구분해서 저장 **/
+                List<List<String>> commandList = new ArrayList<>();
+                List<String> insert = new ArrayList<>();
+                List<String> delete = new ArrayList<>();
+
+                String separater = "I";
+                for (String str : raw) {
+                    if (!Character.isDigit(str.charAt(0))) {
+                        if (!insert.isEmpty()) {
+                            commandList.add(insert);
+                        } else if (!delete.isEmpty()) {
+                            commandList.add(delete);
+                        }
+
+                        separater = str;
+                        insert = new ArrayList<>();
+                        delete = new ArrayList<>();
+                    }
+
+                    if (separater.equals("I")) {
+                        insert.add(str);
+                    } else if (separater.equals("D")) {
+                        delete.add(str);
+                    }
+                }
+                if (!insert.isEmpty()) {//마지막 한번이 누락되어서 추가.
+                    commandList.add(insert);
+                } else if (!delete.isEmpty()) {
+                    commandList.add(delete);
+                }
+
+                /** 문제의 암호화 **/
+                List<String> result = stream.collect(Collectors.toList()); //Arrays.asList하면 addAll이 exception
+                for (int j = 0; j < commands; j++) {
+                    String operation = commandList.get(j).get(0);
+
+                    result = (operation.equals("I")) ? encryptInsert(result, commandList.get(j)) : encrypteDelete(result, commandList.get(j));
+
+                }
+
+                /** 답 프린트 **/
+                System.out.printf("#%d", i + 1);
+                for (int n = 0; n < 10; n++) {
+                    System.out.print(" " + result.get(n));
+                }
+                System.out.println();
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
         }
     }
 
     /**
-     * SW Expert Academy 1230번
+     * SW Expert Academy 1229번
+     *
      * @throws Exception
      */
-    public void no1230 () throws Exception{
+    public static List<String> encryptInsert(List<String> result, List<String> commands) throws Exception {
+        try {
+            int pos = Integer.parseInt(commands.get(1));
+            int count = Integer.parseInt(commands.get(2));
+            int max = count + 3;
+
+            List<String> temp = new ArrayList<>();
+            for (int i = 3; i < max; i++) {
+                temp.add(commands.get(i));
+            }
+
+            result.addAll(pos, temp);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return result;
+    }
+
+    /**
+     * SW Expert Academy 1229번
+     *
+     * @throws Exception
+     */
+    public static List<String> encrypteDelete(List<String> result, List<String> commands) throws Exception {
+        try {
+            int pos = Integer.parseInt(commands.get(1));
+            int count = Integer.parseInt(commands.get(2));
+
+            result.subList(pos, pos + count).clear();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+        return result;
+    }
+
+
+    /**
+     * SW Expert Academy 1230번
+     *
+     * @throws Exception
+     */
+    public void no1230() throws Exception {
         try {
 
-        } catch (Exception e){
+        } catch (Exception e) {
 
         }
     }
